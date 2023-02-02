d1f96d16d22d87f38dfb4dee571445ede364acde
Maxime Daniel <root@maxux.net>
2023-02-02 03:49:31 +0100

    init: add repository clone

--- init.sh
+++ init.sh
@@ -5,4 +5,12 @@ mkdir root/etc
 
 cd root/etc
 ln -s ../../portage
+cd ../..
+
+cd repos
+if [ ! -d gentoo/metadata ]; then
+    git clone --depth=1 https://github.com/maxux/gentoo-portage gentoo
+fi
+cd ..
+
 
